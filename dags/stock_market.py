fcb7fd27a265752c44109cfd3a152eebea73bdc7
Roey Zalta <roey.zalta@gmail.com>
2024-09-29 09:10:23 +0300

    fix functions calling on stock_market.py

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -1,35 +1,40 @@
 from airflow.decorators import dag, task
+from datetime import datetime
 from airflow.hooks.base import BaseHook
 from airflow.sensors.base import PokeReturnValue
 from airflow.operators.python import PythonOperator
-from datetime import datetime
-import requests
 from include.stock_market.tasks import _get_stock_prices
+import requests
 
 SYMBOL = "AAPL"
 
-@dag( 
-     start_date=datetime(2024, 1, 1), 
-     schedule='@daily',
-     catchup=False,
-     tags=['stock_market']
-     )
 
+@dag(
+    start_date=datetime(2023, 1, 1),
+    schedule="@daily",
+    catchup=False,
+    tags=["stock_market"],
+)
 def stock_market():
 
-    @task.sensor(poke_interval=30, timeout=300, mode="poke")
+    @task.sensor(poke_interval=30, timeout=3600, mode="poke")
     def is_api_available() -> PokeReturnValue:
-        api=BaseHook.get_connection("stock_api")
-        url=f"{api.host}{api.extra_dejson['endpoint']}"
-        response=requests.get(url, headers=api.extra_dejson['headers'])
-        condition=response.json()['finance']['result'] is None
+        api = BaseHook.get_connection("stock_api")
+        url = f"{api.host}{api.extra_dejson['endpoint']}"
+        response = requests.get(url, headers=api.extra_dejson["headers"])
+        condition = response.json()["finance"]["result"] is None  # <-- changed
         return PokeReturnValue(is_done=condition, xcom_value=url)
-    
-    get_stock_prices=PythonOperator(
+
+    get_stock_prices = PythonOperator(
         task_id="get_stock_prices",
-        python_callable=get_stock_prices,
-        op_kwargs={"url": "{{ task_instance.xcom_pull(task_ids='is_api_available') }}", "symbol": SYMBOL}
+        python_callable=_get_stock_prices,
+        op_kwargs={
+            "url": '{{ ti.xcom_pull(task_ids="is_api_available") }}',
+            "symbol": SYMBOL,
+        },
     )
-    
+
     is_api_available() >> get_stock_prices
+
+
 stock_market()
